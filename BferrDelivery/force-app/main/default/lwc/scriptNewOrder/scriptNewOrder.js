8aadd4ad94f97077c54fe6bfe90ebf2efec533b6
BiagioFerracane <Biagio.ferracane232@gmail.com>
2023-01-12 11:19:07 +0100

    MetaProgetto

---
+++ BferrDelivery/force-app/main/default/lwc/scriptNewOrder/scriptNewOrder.js
@@ -0,0 +1,60 @@
+import { LightningElement, track } from 'lwc';
+import ORDER_OBJECT from '@salesforce/schema/Order';
+import newOrder from '@salesforce/apex/CreateOrder.newOrder';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class LightOrder extends LightningElement {
+    
+    @track AccountId;
+    @track ShipToContactId;
+    @track Status;
+    @track EffectiveDate;
+
+    handleAccountId(event){
+        this.AccountId=event.target.value;
+    }
+
+    handleShipTo(event){
+        this.ShipToContactId=event.target.value;
+    }
+
+    handleStatus(event){
+        this.Status=event.target.value;
+    }
+
+    handleDate(event){
+        this.EffectiveDate=event.target.value;
+    }
+
+    handleClick(){
+        newOrder({acct : this.AccountId, ship : this.ShipToContactId, state : this.Status, day : this.EffectiveDate})
+        .then(result => {
+            this.message = result;
+            this.error = undefined;
+            if(this.message !== undefined) {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Order created Succesfully',
+                        variant: 'success',
+                    }),
+                );
+            }
+            
+            console.log(JSON.stringify(result));
+            console.log("result", this.message);
+        })
+        .catch(error => {
+            this.message = undefined;
+            this.error = error;
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Failed to Insert order',
+                    message: error.body.message,
+                    variant: 'error',
+                }),
+            );
+            console.log("error", JSON.stringify(this.error));
+        });
+    };
+}
